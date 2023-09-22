408210f74c73396cda995c1400a4c6a4100bd69a
Ahmed Mahmoud <eng.ahmed18@gmail.com>
2023-09-22 14:48:51 -0700

    Gw@3z

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,17 @@
+/*
+ * This File Done By aGw@3z
+ */
+#include "lists.h"
+
+void _constructor(void) __attribute__ ((constructor));
+
+/**
+ * _constructor - executes before main()
+ *By Gw@3z
+ * Return: void
+ */
+void _constructor(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+		"I bore my house upon my back!\n");
+}
